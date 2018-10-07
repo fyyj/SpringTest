b551ba1db643958486bc8d633400cd73968e6bbd
zjq0828 <1531257927@qq.com>
2018-10-07 21:48:00 +0800

    UnitTestBase.java

---
+++ UnitTestBase.java
@@ -0,0 +1,58 @@
+package priv.zjq.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.beans.BeansException;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.StringUtils;
+
+public class UnitTestBase {
+	
+	private ClassPathXmlApplicationContext context;
+	
+	private String springXmlpath;
+	
+	public UnitTestBase() {}
+	
+	public UnitTestBase(String springXmlpath) {
+		this.springXmlpath = springXmlpath;
+	}
+	
+	@Before
+	public void before() {
+		if (StringUtils.isEmpty(springXmlpath)) {
+			springXmlpath = "classpath*:spring-*.xml";
+		}
+		try {
+			context = new ClassPathXmlApplicationContext(springXmlpath.split("[,\\s]+"));  //正则表达式
+			context.start();								//\\s表示 空格,回车,换行等空白符, +表示一个或多个
+		} catch (BeansException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@After
+	public void after() {
+		context.destroy();
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected <T extends Object> T getBean(String beanId) {
+		try {
+			return (T)context.getBean(beanId);
+		} catch (BeansException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	protected <T extends Object> T getBean(Class<T> clazz) {
+		try {
+			return context.getBean(clazz);
+		} catch (BeansException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+}
